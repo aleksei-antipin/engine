60edff2fa22e22ad91cb0ba93ab3b16d7b93960c
Aleksei <114477571+aleksei-antipin@users.noreply.github.com>
2025-03-16 20:29:56 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "src/Engine.h"
+
+int main() {
+    auto *engine = new Engine();
+    engine->Run();
+    return 0;
+}
---
+++ src/Engine.h
@@ -0,0 +1,10 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+class Engine {
+public:
+    void Run();
+
+private:
+    GLFWwindow *window;
+};
